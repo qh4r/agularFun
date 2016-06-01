b91327945f3dfc318cc555961b1c80b1ee6b2eff
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-06-01 21:27:01 +0200

    Played around with modules and services

--- reminder/app.js
+++ reminder/app.js
@@ -1,6 +1,23 @@
-var reminderApp  = angular.module('reminderApp', []);
+var reminderApp  = angular.module('reminderApp', ['ngMessages', 'ngResource']);
 
 
-reminderApp.controller('mainController', function(){
+// tablia zamiast samej funkcji podawana jest w celu minifikacji
+//Stringi nie sa minifikowane przez co angular wie ktora zmienna jest czym
+reminderApp.controller('mainController', ['$scope', '$log', '$resource', '$timeout',
+    function($scope, $log, $resource, $timeout){
 
-});
+    $log.info('dupa');
+    $scope.dupa = 'ads';
+    $scope.dupaFunction = function(){
+        return 'funkcja dupa'
+    }
+
+        $timeout(function(){
+            $scope.dupa = ' zmiany i heheszki';
+        }, 2000)
+
+    //return {scope: $scope};
+
+}]);
+
+//var reminderApp=angular.module("reminderApp",["ngMessages","ngResource"]);reminderApp.controller("mainController",["$scope","$log","$resource",function(e,n,r){return n.info("dupa"),e.dupa="ads",e.dupaFunction=function(){return"funkcja dupa"},{scope:e}}]);
