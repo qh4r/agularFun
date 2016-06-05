ad4b538bedb162d395353c22ce29b51ce6c85747
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-06-05 02:13:45 +0200

    Played around with custom service

--- reminder/routing.js
+++ reminder/routing.js
@@ -1,5 +1,11 @@
 var routingApp = angular.module('routingApp', ['ngRoute']);
 
+routingApp.service('testService', function () {
+    this.testText = 'testowa wartość';
+    this.getLength = function () {
+        return this.testText.length;
+    }.bind(this);
+});
 
 routingApp.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
@@ -7,34 +13,46 @@ routingApp.config(['$routeProvider', function ($routeProvider) {
             templateUrl: 'views/home.html',
             controller: 'routingMainController'
         })
-        .when('/asd',{
+        .when('/asd', {
             templateUrl: 'views/asd.html',
             controller: 'asdStubController'
         })
-        .when('/dupa/:val',{
+        .when('/dupa/:val', {
             templateUrl: 'views/dupa.html',
             controller: 'dupaStubController'
         });
 }]);
-routingApp.controller('routingMainController', ['$scope', '$log', '$location',
-    function ($scope, $log, $location) {
+routingApp.controller('routingMainController', ['$scope', '$log', '$location', 'testService',
+    function ($scope, $log, $location, testService) {
         console.log('HOME');
         window.addEventListener('hashchange', function () {
             $log.log($location.path());
             // $scope.$apply(function(){
             // })
         });
+        //SERWIS TO SINGELTON WIEC REFERUJE TEN SAM OBIEKT WE WSZYSTKICH KONTROLERACH
+        $scope.changeValue = function() {
+            testService.testText = 'inna wartość';
+            $scope.testText = testService.getLength();
+        };
+        $scope.testText = testService.getLength();
 
     }]);
 
-routingApp.controller('asdStubController', ['$scope', '$log', '$location',
-    function($scope, $log, $location){
+routingApp.controller('asdStubController', ['$scope', '$log', '$location' ,'testService',
+    function ($scope, $log, $location, testService) {
         $scope.test = 'testowy string sobie';
-        console.log('asd!')
+        console.log('asd!');
+        $scope.testText = testService.testText;
     }]);
 
-routingApp.controller('dupaStubController', ['$scope', '$log', '$location', '$routeParams',
-    function($scope, $log, $location, $routeParams){
-        console.log('dupa!')
+routingApp.controller('dupaStubController', ['$scope', '$log', '$location', '$routeParams', 'testService',
+    function ($scope, $log, $location, $routeParams, testService) {
+        console.log('dupa!');
         $scope.param = $routeParams.val;
+        $scope.testBinding = testService.testText;
+
+        $scope.$watch('testBinding', function(newValue){
+           testService.testText = newValue;
+        });
     }]);
