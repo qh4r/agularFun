e1810a7f6c5c601bcf133390887990748d74843d
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-06-06 01:11:39 +0200

    Poked around with custom directives and their arguments

--- reminder/routing.js
+++ reminder/routing.js
@@ -30,7 +30,33 @@ routingApp.directive('testCustomDirective', function(){
         // template: '<a href="#/dupa/1" class="list-group-item"><h4 class="list-group-item-heading">Rafał</h4>' +
         // '<p class="list-group-item-text">test test</p></a>',
         templateUrl: 'templates/testCustomDirective.html',
-        replace: true //REPLACE SPRAWIA  Z DOM ZNIKA TAG (KONTENER) Z NAZWA DYREKTYWY
+        replace: true, //REPLACE SPRAWIA  Z DOM ZNIKA TAG (KONTENER) Z NAZWA DYREKTYWY
+        scope: { //IZOLACJA ZAKRESU -- NADPISUJE ZAKRES KONTROLERA
+            personName: '@', //@ oznacza BINDING JEDNOSTRONNY - do dyrektywy
+            personDescription: '@',
+            personId: '@'
+        }
+    }
+});
+
+//NIE NADPISUJE SCOPE WIEC KORZYSTA ZE SCOPE CONTROLERA
+routingApp.directive('forShowDirective', function(){
+   return {
+       restrict: 'E',
+       templateUrl: 'templates/forShowDirective.html',
+       replace: true
+   }
+});
+
+routingApp.directive('cleanForShowDirective', function(){
+    return {
+        restrict: 'E',
+        templateUrl: 'templates/cleanForShowDirective.html',
+        replace: true,
+        scope: {
+            cityObject: '=', // = -- oznacza obiekt -- TO 2 STRONNY BINDING
+            cityFunction: '&' // & -- oznacza funkcje
+        }
     }
 });
 
@@ -49,6 +75,14 @@ routingApp.controller('routingMainController', ['$scope', '$log', '$location', '
         };
         $scope.testText = testService.getLength();
 
+        $scope.people = [
+            {id: 11, name: 'Rafał', description: 'Przystojny błyskotliwy wygadany'},
+            {id: 22, name: 'Kamil', description: 'Może być ale maruda'},
+            {id: 33, name: 'Szymon', description: 'Troche gbór'},
+            {id: 44, name: 'Asia', description: 'Słodka ale w sumie też maruda'},
+            //z kim ja sie zadaje
+        ]
+
     }]);
 
 routingApp.controller('asdStubController', ['$scope', '$log', '$location' ,'testService',
@@ -56,6 +90,22 @@ routingApp.controller('asdStubController', ['$scope', '$log', '$location' ,'test
         $scope.test = 'testowy string sobie';
         console.log('asd!');
         $scope.testText = testService.testText;
+
+        $scope.cities = [
+            {name: 'Czikago', population: 12000000},
+            {name: 'Ditroit', population: 2410000},
+            {name: 'Miszigan', population: 592800},
+            {name: 'Palo alto', population: 3344002},
+            {name: 'Gliwice', population: 160000},
+        ];
+
+        $scope.getCity = function(city) {
+            return (function(result){
+                console.log(result);
+                return result;
+            })(city.name + ' == > ' + city.population);
+        }
+
     }]);
 
 routingApp.controller('dupaStubController', ['$scope', '$log', '$location', '$routeParams', 'testService',
