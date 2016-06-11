ab43aa0d171b076365badf1e1841cef31a5e616f
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-06-12 00:34:46 +0200

    Minor tweaks and improvements

--- simpleApp/controllers/homeController.js
+++ simpleApp/controllers/homeController.js
@@ -1,7 +1,10 @@
-simpleApp.controller('homeController', ['$scope', '$citySelection',
-    function ($scope, $citySelection) {
+simpleApp.controller('homeController', ['$scope', '$citySelection', '$location',
+    function ($scope, $citySelection, $location) {
         $scope.selectedCity = $citySelection.getCityName();
         $scope.$watch('selectedCity', function(newValue){
             $citySelection.updateCityName(newValue);
         })
+        $scope.submit = function(){
+            $location.path('/forecast/3')
+        }
     }]);
